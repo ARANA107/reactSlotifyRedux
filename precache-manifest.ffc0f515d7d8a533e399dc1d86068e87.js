0bb02ad9f76df46544ec6103f1ffcfed87076340
Ash Rana <ashrana1993@gmail.com>
2020-08-13 09:54:13 -0500

    Updates

---
+++ precache-manifest.ffc0f515d7d8a533e399dc1d86068e87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4cc17b5b50aba1cb9b14f4ce0804bff",
+    "url": "/reactSlotifyRedux/index.html"
+  },
+  {
+    "revision": "c59c8c4cf6962d97f35e",
+    "url": "/reactSlotifyRedux/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1f9449c664b38afbaeff",
+    "url": "/reactSlotifyRedux/static/css/main.5b3296a3.chunk.css"
+  },
+  {
+    "revision": "c59c8c4cf6962d97f35e",
+    "url": "/reactSlotifyRedux/static/js/2.0b262da1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactSlotifyRedux/static/js/2.0b262da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9449c664b38afbaeff",
+    "url": "/reactSlotifyRedux/static/js/main.47a2d7c5.chunk.js"
+  },
+  {
+    "revision": "660e50c44211764fab64",
+    "url": "/reactSlotifyRedux/static/js/runtime-main.665284c2.js"
+  }
+]);
